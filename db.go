8af84dd9b4a21b99e6f1cf149ae7e68f7c66884a
Vasiliy Solovey <solovey@cinegy.com>
2017-02-13 16:58:33 +0200

    Download DynamoDB archive and extract it on first launch Also fixed bug on Windows with not working Close() method.

--- db.go
+++ db.go
@@ -8,16 +8,18 @@ import (
 	"net"
 	"os"
 	"os/exec"
-	"strings"
 	"sync/atomic"
-	"syscall"
 	"time"
+	"net/http"
+	"io"
+	"archive/tar"
+	"compress/gzip"
 )
 
 var (
-	ConnectTimeout    = 10 * time.Second
+	ConnectTimeout = 10 * time.Second
 	ErrConnectTimeout = errors.New("[dynamodbtest] timeout starting server")
-	ErrGopath         = errors.New("[dynamodbtest] GOPATH must be set")
+	ErrGopath = errors.New("[dynamodbtest] GOPATH must be set")
 )
 
 // LogOutput must be set before calling New()
@@ -29,23 +31,104 @@ type DB struct {
 	cmd  *exec.Cmd
 }
 
+func read(mpath string) (*os.File, error) {
+	f, err := os.OpenFile(mpath, os.O_RDONLY, 0444)
+	if err != nil {
+		return f, err
+	}
+	return f, nil
+}
+
+func overwrite(mpath string) (*os.File, error) {
+	f, err := os.OpenFile(mpath, os.O_RDWR | os.O_TRUNC, 0777)
+	if err != nil {
+		f, err = os.Create(mpath)
+		if err != nil {
+			return f, err
+		}
+	}
+	return f, nil
+}
+
+func untarIt(mpath string) {
+	fr, err := read(mpath)
+	defer fr.Close()
+	if err != nil {
+		panic(err)
+	}
+	gr, err := gzip.NewReader(fr)
+	defer gr.Close()
+	if err != nil {
+		panic(err)
+	}
+	tr := tar.NewReader(gr)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			// end of tar archive
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		path := hdr.Name
+		switch hdr.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(path, os.FileMode(hdr.Mode)); err != nil {
+				panic(err)
+			}
+		case tar.TypeReg:
+			ow, err := overwrite(path)
+			defer ow.Close()
+			if err != nil {
+				panic(err)
+			}
+			if _, err := io.Copy(ow, tr); err != nil {
+				panic(err)
+			}
+		default:
+			fmt.Printf("Can't: %c, %s\n", hdr.Typeflag, path)
+		}
+	}
+}
+
 // New returns a started DynamoDB local instance
 func New() (*DB, error) {
 	port := newPort()
 	addr := fmt.Sprintf("localhost:%d", port)
 	// if $GOPATH is composed of multiple paths, use the first one (fix for godep)
-	gopath := strings.Split(os.Getenv("GOPATH"), ":")[0]
+	gopath := os.Getenv("GOPATH")
 	if gopath == "" {
 		return nil, ErrGopath
 	}
-	path := gopath + "/src/github.com/groupme/dynamodbtest/"
+	path := gopath + "/src/github.com/miltador/dynamodbtest/"
+	archivePath := path + "dynamodb_local_latest.tar.gz"
+	if _, err := os.Stat(archivePath); os.IsNotExist(err) {
+		response, err := http.Get("https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz")
+		if err != nil {
+			panic(err)
+		}
+		defer response.Body.Close()
+
+		f, err := os.Create(archivePath)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		io.Copy(f, response.Body)
+	}
+	if _, err := os.Stat(path + "DynamoDbLocal_lib/"); os.IsNotExist(err) {
+		untarIt(archivePath)
+	}
+
 	db := &DB{
 		addr: addr,
 		cmd: exec.Command(
 			"java",
-			"-Djava.library.path="+path+"DynamoDbLocal_lib",
+			"-Djava.library.path=" + path + "DynamoDbLocal_lib",
 			"-jar",
-			path+"DynamoDBLocal.jar",
+			path + "DynamoDBLocal.jar",
 			"-port",
 			fmt.Sprintf("%d", port),
 			"-inMemory",
@@ -78,7 +161,7 @@ func New() (*DB, error) {
 	go func() {
 		// periodically check if connectable
 		ticker := time.NewTicker(time.Millisecond * 10)
-		for _ = range ticker.C {
+		for range ticker.C {
 			c, err := net.Dial("tcp", db.addr)
 			if c != nil {
 				c.Close()
@@ -99,8 +182,7 @@ func New() (*DB, error) {
 }
 
 func (db *DB) Close() error {
-	db.cmd.Process.Signal(syscall.SIGINT)
-	return db.cmd.Wait()
+	return db.cmd.Process.Kill()
 }
 
 func (db *DB) URL() string {
